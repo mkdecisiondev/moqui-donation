2ef0eab51dc56fae46e1cb2f6caa87388717287a
jeromerrr <jerome.shep@gmail.com>
2018-12-06 08:55:36 -0800

    redesigned thank you page is done, cleaned up code in general, minor UI tweaks on first time donor page

--- screen/GND/handleGND.js
+++ screen/GND/handleGND.js
@@ -1,44 +1,11 @@
-// //Radio buttons have same name
-// //other has different name
-
-// //Used by radio buttons
-// function clearOtherAmount(){
-//     document.getElementById('amount').value = ''
-// }
-
-// //used by $other button/input
-// function selectOtherRadioButton(){
-//     document.getElementById("rdo-amount-other").checked = true
-// }
-
-// function focusOnOtherAmount(){
-//     const otherAmount = document.getElementById("amount")
-//     otherAmount.focus()
-//     otherAmount.select()
-// }
-
-
-function checkDonationAmount(){
-   let x = document.getElementsByName("DonationAmount")
-   
-   console.log("Elements " + document.getElementsByName("DonationAmount"));
-   
-   console.log("Elements value = " + x);
-   
-   let y = document.getElementById("amountBox1")
-
-   console.log(y);
-   console.log(y.value);
-   
-}
-
-
+//Checks the radio button when selecting the input field that is on top of it
 function setOtherAmount(){
     document.getElementById("otherAmountRadio").value = document.getElementById("otherAmtNumberInput").value 
 
     // console.log("VALUE::: " + document.getElementById("otherAmountRadio").value);   
 }
 
+//Sets the value of the $Other radio button based on what user inputs in the input field that is on top of it
 function checkOtherRadio(){
     document.getElementById("otherAmountRadio").checked = true
 
